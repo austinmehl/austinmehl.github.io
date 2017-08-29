29d338904993c9ac54dc362054f8447eecfe7f6f
austinmehl <austin.mehl@gmail.com>
2017-08-29 17:25:20 -0400

    different code attempt

--- javascript/movingFun2.js
+++ javascript/movingFun2.js
@@ -1,5 +1,6 @@
-$(function(){
-  //saving dom objects to variables 
+$(function () {
+
+    //saving dom objects to variables
     var container = $('#container');
     var bird = $('#bird');
     var pole = $('.pole');
@@ -8,10 +9,8 @@ $(function(){
     var score = $('#score');
     var speed_span = $('#speed');
     var restart_btn = $('#restart_btn');
-    
-    
-    //saving some intial setup
-    
+
+    //saving some initial setup
     var container_width = parseInt(container.width());
     var container_height = parseInt(container.height());
     var pole_initial_position = parseInt(pole.css('right'));
@@ -19,84 +18,65 @@ $(function(){
     var bird_left = parseInt(bird.css('left'));
     var bird_height = parseInt(bird.height());
     var speed = 10;
-    
-    
+
     //some other declarations
     var go_up = false;
     var score_updated = false;
     var game_over = false;
-    
-    
-    
-    var the_game = setInterval(function(){
-        
-        if (collision(bird, pole_1) || collision(bird,pole_2) || parseInt(bird.css('top'))<= 0 || parseInt(bird.css('top'))> container_height - bird_height){
-            
+
+
+    var the_game = setInterval(function () {
+
+        if (collision(bird, pole_1) || collision(bird, pole_2) || parseInt(bird.css('top')) <= 0 || parseInt(bird.css('top')) > container_height - bird_height) {
+
             stop_the_game();
-        }
-            
-            
-        else{
-        
-        var pole_current_position = parseInt(pole.css('right'));
-        
-            
-            // update the score when the poles have passed the bird successfully
-            
-            if (pole_current_position > container_width - bird_left){
+
+        } else {
+
+            var pole_current_position = parseInt(pole.css('right'));
+
+            //update the score when the poles have passed the bird successfully
+            if (pole_current_position > container_width - bird_left) {
                 if (score_updated === false) {
-                    score.text(parseInt(score.text()) +1);
+                    score.text(parseInt(score.text()) + 5);
                     score_updated = true;
                 }
             }
-        
-        //check if poles go out of the container 
-        if(pole_current_position > container_width){
-            var new_height = parseInt(Math.random()*120);
-            
-            
-            //change poles height
-          pole_1.css('height', pole_initial_height + new_height)
-            pole_2.css('height', pole_initial_height - new_height)
-            
-        
-            
-            
-            //change speed
-            speed = speed + 2;
-            speed_span.text(speed);
-            
-            
-            score_updated = false;
-            
-            
-            pole_current_position = pole_initial_position;
+
+            //check whether the poles went out of the container
+            if (pole_current_position > container_width) {
+                var new_height = parseInt(Math.random() * 100);
+
+                //change the pole's height
+                pole_1.css('height', pole_initial_height + new_height);
+                pole_2.css('height', pole_initial_height - new_height);
+
+                //increase speed
+                speed = speed + 3;
+                speed_span.text(speed);
+
+                score_updated = false;
+
+                pole_current_position = pole_initial_position;
+            }
+
+            //move the poles
+            pole.css('right', pole_current_position + speed);
+
+            if (go_up === false) {
+                go_down();
+            }
         }
-        
-        // move the poles 
-        pole.css('right',pole_current_position + speed);
-        
-        
-        if (go_up === false){
-            go_down();
-        }
-            
-        }
-        
-    },40);
-    
-    
-    
+
+    }, 40);
+
     $(document).on('keydown', function (e) {
         var key = e.keyCode;
-        if (key === 32 && go_up===false && game_over === false) {
-            
-            go_up = setInterval(up,50);
+        if (key === 32 && go_up === false && game_over === false) {
+            go_up = setInterval(up, 25);
         }
     });
-    
-    
-    
+
     $(document).on('keyup', function (e) {
         var key = e.keyCode;
         if (key === 32) {
@@ -104,22 +84,17 @@ $(function(){
             go_up = false;
         }
     });
-    
-    
-    
-    function go_down (){
-        bird.css('top',parseInt(bird.css('top'))+8);
+
+
+    function go_down() {
+        bird.css('top', parseInt(bird.css('top')) + 5);
     }
-    
-    
-    function up(){
-         bird.css('top',parseInt(bird.css('top'))-11);
+
+    function up() {
+        bird.css('top', parseInt(bird.css('top')) - 10);
     }
-    
-    
-    //mobile ability 
-    
-       $(document).on('click touchstart', function (e) {
+
+    $(document).on('taphold', function (e) {
         var taphold = e.type;
         if (taphold && go_up === false) {
             go_up = setInterval(up, 20);
@@ -127,22 +102,15 @@ $(function(){
     });
 
 
-
-
-    $(document).on('click touchstart', function (e) {
+    $(document).on('tap', function (e) {
         var tap = e.type;
         if (tap) {
             clearInterval(go_up)
             go_up = false;
         }
     });
-    //end mobile ability 
-    
-    
-    
-    
-    // check mouse down
-      $(document).on('mousedown', function (e) {
+
+    $(document).on('mousedown', function (e) {
         var mouse = e.type;
         if (mouse && go_up === false) {
             go_up = setInterval(up, 50);
@@ -157,32 +125,18 @@ $(function(){
             go_up = false;
         }
     });
-    
-    //end mouse down
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
     function stop_the_game() {
         clearInterval(the_game);
         game_over = true;
         restart_btn.slideDown();
     }
-    
-    
-    
-    restart_btn.click(function(){
+
+    restart_btn.click(function () {
         location.reload();
-        
     });
-    
-    
-       function collision($div1, $div2) {
+
+    function collision($div1, $div2) {
         var x1 = $div1.offset().left;
         var y1 = $div1.offset().top;
         var h1 = $div1.outerHeight(true);
@@ -200,6 +154,6 @@ $(function(){
         return true;
     }
 
-    
-    
+
+
 });
